1d398b54878e2de605570fd7b5b07ca1f882f590
Jakub Depka <jdepka@c3r4s1.42warsaw.pl>
2024-03-23 13:50:22 +0100

    gnl

--- getnextline/get_next_line.c
+++ getnextline/get_next_line.c
@@ -90,7 +90,7 @@ char	*get_next_line(int fd)
 	static char	*buffer;
 	char		*line;
 
-	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (!buffer)
 		buffer = ft_calloc(1, 1);
