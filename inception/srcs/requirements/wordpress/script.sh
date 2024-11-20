fbf8454f0676466d46f7ef02b3cc1dae3889c882
jdepka <jdepka@42Warsaw.myguest.virtualbox.org>
2024-11-20 16:37:47 +0100

    inc2

--- inception/srcs/requirements/wordpress/script.sh
+++ inception/srcs/requirements/wordpress/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # create directory to use in nginx container later and also to setup the wordpress conf
-mkdir /var/www/
-mkdir /var/www/html
+mkdir -p /var/www/
+mkdir -p /var/www/html
 
 cd /var/www/html
 
-rm -rf *
+rm -rf /var/www/html/*
 
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar 
 
@@ -26,13 +26,10 @@ sed -i -r "s/pwd/$db_pwd/1"    wp-config.php
 wp core install --url=$DOMAIN_NAME/ --title=$WP_TITLE --admin_user=$WP_ADMIN_USR --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USR $WP_EMAIL --role=author --user_pass=$WP_PWD --allow-root
 wp theme install astra --activate --allow-root
-wp plugin install redis-cache --activate --allow-root
 wp plugin update --all --allow-root
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/g' /etc/php/7.3/fpm/pool.d/www.conf
 
 mkdir /run/php
-
-wp redis enable --allow-root
 
 /usr/sbin/php-fpm7.3 -F
